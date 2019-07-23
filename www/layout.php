3b5eec63a1bf704e4087bbd3fb6251f7f3b3a5ac
Mélanie V <melanie.vaubien@hotmail.com>
2019-07-23 16:15:02 +0200

    Ajout d'un fichier Layout

---
+++ www/layout.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="front/css/site.css"/>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <title>CodeContest</title>
+</head>
